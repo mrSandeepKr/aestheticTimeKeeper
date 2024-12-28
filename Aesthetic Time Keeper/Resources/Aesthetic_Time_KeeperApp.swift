6a2e34ef12a7741a4758efe0df0ca12ef4933b26
Sandeep Kumar <sandeep.kumar.ece16@itbhu.ac.in>
2024-12-28 15:16:44 +0530

    Initial commit

---
+++ Aesthetic Time Keeper/Resources/Aesthetic_Time_KeeperApp.swift
@@ -0,0 +1,18 @@
+//
+//  Aesthetic_Time_KeeperApp.swift
+//  Aesthetic Time Keeper
+//
+//  Created by Sandeep Kumar on 28/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Aesthetic_Time_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aesthetic Time Keeper/Resources/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Aesthetic Time Keeper
+//
+//  Created by Sandeep Kumar on 28/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Select an item")
+    }
+}
